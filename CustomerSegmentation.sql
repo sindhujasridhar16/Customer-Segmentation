7bd26e82dfa74d8504e5f936f893c8f96f3e7e5a
sindhujasridhar16 <161644360+sindhujasridhar16@users.noreply.github.com>
2024-02-28 16:44:54 -0500

    Update CustomerSegmentation.sql

--- CustomerSegmentation.sql
+++ CustomerSegmentation.sql
@@ -1,7 +1,9 @@
 -- Explore the Data
 -- Inspect the data, we have to make sure the columns required for RFM analysis exist 
-(last purchase date, total orders, and total money spent).
+-- (last purchase date, total orders, and total money spent).
 -- In this case, we use MySQL Workbench and query.
 select customer_id, gender, age, total_orders, price, payment_method, shopping_mall,
 substring_index(invoice_date, ' ', 1) invoice_date
 from customer_shopping_data;
+
+
